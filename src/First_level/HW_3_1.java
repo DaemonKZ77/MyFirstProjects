6261f2b17fb46674ac2cfb57f17c317285bc83d2
DaemonKZ77 <dkutnyakov@gmail.com>
2022-08-26 08:45:12 +0600

    Commit1

--- src/First_level/HW_3_1.java
+++ src/First_level/HW_3_1.java
@@ -6,6 +6,7 @@ public class HW_3_1 {
         int b = 5;
         System.out.println("Метод Sum - " + sum(a, b));
         System.out.println("Метод Mutliply - " + multiply (a, b));
+        System.out.println("Метод Subt - " + subt (a, b));
         System.out.println("Метод Devision - " + div (a, b));
     }
 
@@ -13,12 +14,14 @@ public class HW_3_1 {
         int result = a + b;
         return result;
     }
-
     public static int multiply(int a, int b) {
         int result = a * b;
         return result;
     }
-
+    public static int subt (int a, int b) {
+        int result = a-b;
+        return result;
+    }
     public static float div (int a, int b) {
         float result = a/b;
         return result;

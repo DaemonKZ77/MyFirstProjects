11f9abb852688cb95f66116c2f36737e5a4d4fdf
DaemonKZ77 <dkutnyakov@gmail.com>
2022-08-26 09:57:09 +0600

    Commit1

--- src/First_level/HW_8_1.java
+++ src/First_level/HW_8_1.java
@@ -11,8 +11,14 @@ public class HW_8_1 {
         String cost = string.readLine();
         info(cost);
 
+        info_1(string.readLine(), Integer.parseInt(string.readLine()),Integer.parseInt(string.readLine()));
+
     }
     private static void info (String a) {
         System.out.println("Я зарабатываю - " + a + "$ в час " );
+    }
+
+    public static void info_1 (String name, int cost, int year)    {
+        System.out.println(name + "Будет получить " + cost + " через " + year + " лет");
     }
 }
